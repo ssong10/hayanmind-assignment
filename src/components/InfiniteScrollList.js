1d2d4b3ea5bc5196eecd9e11cf365264f7c42e67
ssong10 <leesy1403@naver.com>
2021-07-27 20:04:50 +0900

    Feat: InfiniteScrollList - comment 컴포넌트 적용

--- src/components/InfiniteScrollList.js
+++ src/components/InfiniteScrollList.js
@@ -1,46 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { fetchComments } from '../utils/api';
+import Comment from './comment'
 
 const InfiniteScrollList = () => {
-  const [page,setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
   const [comments, setComments] = useState([]);
   const CARD_HEIGHT = 500
-  useEffect(async ()=>{
-    setLoading(true)
-    console.log(page)
-    await getComments(page)
-    setLoading(false)
+  useEffect(()=>{
+    getComments(page)
   },[page])
   const onScroll = (e) => {
-    if (!loading) {
-      const {
-        scrollHeight,
-        scrollTop,
-        clientHeight
-      } = e.target.scrollingElement;
-      if (scrollTop + clientHeight + (CARD_HEIGHT)/2 > scrollHeight) {
-        setPage(page => page+1)
-      }
+    const {
+      scrollHeight,
+      scrollTop,
+      clientHeight
+    } = e.scrollingElement;
+    if (scrollTop + clientHeight + (CARD_HEIGHT*1.5) > scrollHeight) {
+      setPage(page=>page+1)
     }
   }
-  const getComments = async () => {
-    setLoading(true)
+  const getComments = async (page) => {
     const data = await fetchComments(page);
-    console.log(data);
     setComments((prev) => [...prev, ...data]);
-    setLoading(false)
   }
 
   useEffect(() => {
-    document.addEventListener('scroll', onScroll)
+    document.addEventListener('scroll',onScroll)
     return () => {
       document.removeEventListener('scroll',onScroll)
     }
   },[])
 
   return (
-    <div style={{height:(2000*page)+"px"}}>
+    <div>
+      {comments.map((comment) => 
+        <Comment key={comment.id} comment={comment} />
+      )} 
     </div>
   );
 }
