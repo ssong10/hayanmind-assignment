d7ccda7e017d53ddb491898b23b9d3e82c3ffc6b
ssong10 <leesy1403@naver.com>
2021-07-27 17:06:58 +0900

    Feat: InfiniteScrollList - getComments, page useEffect 구현

--- src/components/InfiniteScrollList.js
+++ src/components/InfiniteScrollList.js
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchComments } from '../utils/api';
 
 const InfiniteScrollList = () => {
+  const [page,setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [comments, setComments] = useState([]);
+  const CARD_HEIGHT = 500
+  useEffect(async ()=>{
+    setLoading(true)
+    console.log(page)
+    await getComments(page)
+    setLoading(false)
+  },[page])
   const onScroll = (e) => {
-    console.log(e)
+    if (!loading) {
+      const {
+        scrollHeight,
+        scrollTop,
+        clientHeight
+      } = e.target.scrollingElement;
+      if (scrollTop + clientHeight + (CARD_HEIGHT)/2 > scrollHeight) {
+        setPage(page => page+1)
+      }
+    }
   }
+  const getComments = async () => {
+    setLoading(true)
+    const data = await fetchComments(page);
+    console.log(data);
+    setComments((prev) => [...prev, ...data]);
+    setLoading(false)
+  }
+
   useEffect(() => {
     document.addEventListener('scroll', onScroll)
     return () => {
       document.removeEventListener('scroll',onScroll)
     }
   },[])
+
   return (
-    <div style={{height:1200+"px"}}>
+    <div style={{height:(2000*page)+"px"}}>
     </div>
   );
 }
